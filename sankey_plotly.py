a8619f745dc70b100e30965c0158bb1e4e2824d8
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-12-03 05:48:32 +0100

    feat: working sankey plot

--- sankey_plotly.py
+++ sankey_plotly.py
@@ -1,6 +1,9 @@
 #%%
 import pandas as pd
 import plotly.graph_objects as go
+import pandas as pd
+from pysankey import sankey
+import matplotlib.pyplot as plt
 
 #%%
 df = pd.read_csv('classification.tsv', sep='\t')
@@ -8,6 +11,10 @@ df = pd.read_csv('classification.tsv', sep='\t')
 #%%
 # Count all unique pairs
 flows = df.groupby(['lineage', 'clade']).size()
+#%%
+a = flows.index.get_level_values(0)
+b = flows.index.get_level_values(1)
+weight = list(flows)
 #%%
 lineage = set(flows.index.get_level_values('lineage').unique())
 clade = set(flows.index.get_level_values('clade').unique())
@@ -32,4 +39,12 @@ fig = go.Figure(data=[go.Sankey(
 
 fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
 fig.show()
+# %%
+# %%
+ax = sankey(
+      left=a[:300], right=b[:300],
+      rightWeight=weight[:300], leftWeight=weight[:300], fontsize=20
+)
+plt.gcf().set_size_inches(40,40)
+plt.savefig('sankey.pdf', bbox_inches='tight',transparent=False) # to save
 # %%
