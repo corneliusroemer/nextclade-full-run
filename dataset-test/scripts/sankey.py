7e8da4408890333bc5cd2654e017fb21bea30b84
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-12-06 20:29:02 +0100

    feat: produce table and sankey of clade changes

--- dataset-test/scripts/sankey.py
+++ dataset-test/scripts/sankey.py
@@ -1,8 +1,10 @@
 #%%
 import pandas as pd
 import pandas as pd
-from pysankey import sankey
+from pysankey2.utils import setColorConf
+from pysankey2 import Sankey
 import matplotlib.pyplot as plt
+
 
 #%%
 old = "nextclade_old_clades.tsv"
@@ -12,20 +14,36 @@ df_new = pd.read_csv(new, sep='\t').set_index('seqName')
 df_old
 #%%
 df = df_old.join(df_new, how='inner',lsuffix='_old', rsuffix='_new')
+# df.sort_values(by=['clade_old','clade_new'],inplace=True,ascending=True)
 df
 #%%
 # Count all unique pairs
-flows = df.groupby(['clade_old', 'clade_new']).size()
-flows
+flows = pd.DataFrame(df.groupby(['clade_old', 'clade_new']).size(),columns=['count'])
+flows.reset_index(inplace=True)
+
 #%%
-a = flows.index.get_level_values(0)
-b = flows.index.get_level_values(1)
-weight = list(flows)
-# %%
-ax = sankey(
-      left=a[:300], right=b[:300],
-      rightWeight=weight[:300], leftWeight=weight[:300], fontsize=20
-)
-plt.gcf().set_size_inches(40,40)
+old_count = df_old.value_counts()
+new_count = df_new.value_counts()
+count_all = old_count.sum()
+#%% 
+share_old = []
+share_new = []
+share_all = []
+for index,data in flows.iterrows():
+      share_old.append(data[2]/old_count[data[0]])
+      share_new.append(data[2]/new_count[data[0]])
+      share_all.append(data[2]/count_all)
+#%%
+flows.loc[:,'share_old']=share_old
+flows.loc[:,'share_new']=share_new
+flows.loc[:,'share_all']=share_all
+#%%
+flows[flows['clade_old'] != flows['clade_new']].to_csv('clade_changes.tsv', sep='\t',float_format='%.2g')
+#%%
+df_old.value_counts()
+#%%
+#%%
+sky = Sankey(df)
+fig,ax = sky.plot()
+plt.gcf().set_size_inches(10,10)
 plt.savefig('sankey.pdf', bbox_inches='tight',transparent=False) # to save
-# %%
